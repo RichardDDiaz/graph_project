c51783242fb9a975a3fe63a3ae050a9decd467a2
RichardDDiaz <richardddiaz98@gmail.com>
2022-04-01 01:16:29 -0300

    lectura de lados funcional

--- parser.c
+++ parser.c
@@ -44,7 +44,6 @@ void run_parser(Grafo g){
     /* recorre los comentarios cuando el primer caracter es 'c' 
        cuando el primer caracter sea != de 'c', en teoria ya estamos
        en la parte de lectura de los lados */
-
     while(!feof(file) && firstchar == 'c'){
         while(firstchar != EOF && firstchar != '\n'){
             fscanf(file,"%c", &firstchar);
@@ -57,18 +56,54 @@ void run_parser(Grafo g){
     u32 mlado;
     char pseudo_edge[5]; // +1 por caracter de temrinación
     // si el primer caracter es EOF, no hay mas datos
-
-    assert(firstchar == 'p' && fscanf(file,"%s %lu %lu",&pseudo_edge , &nver, &mlado) > 0
-            && "Primer caracter no es 'p' o no formato (p n m)" ); //&pseudo_edge[0]
-    assert(!strcmp("edge",pseudo_edge));
+    assert(firstchar == 'p' 
+            && fscanf(file,"%s %lu %lu",pseudo_edge , &nver, &mlado) > 0
+            && "Primer caracter no es 'p' o no formato (p edge n m)");
+    assert(!strcmp("edge",pseudo_edge) && "no es edge, formato incorrecto");
 
     printf("%lu vertices y %lu Lados\n", nver, mlado);
 
     g->nver = nver;
     g->mlados = mlado;
     g->vertices = malloc(sizeof(Vertice) * nver);
-    g->vertOrdNat = malloc(sizeof(Vertice)* nver);
+    g->vertOrdNat = malloc(sizeof(Vertice) * nver);
 
+    // lectura de los lados con vertices
+    // formato de cada linea: e v1 v2
+    u32 vA, vB;
+    u32 count_m = 0;
+    while(count_m <= mlado-1){
+        /*
+            cuando hacemos fscanf(file,"%s %lu %lu",pseudo_edge , &nver, &mlado)
+            el siguiente fscanf tomara el salto de linea de la linea j-1
+            y el siguiente fscanf sera el primer char de la linea j.
+            no se como hacer para que lo ignore sin hacer que lo asigne a una 
+            variable que no usaremos(lo probe con la var u32 vA y tampoco)
+        */
+        fscanf(file,"%c", &firstchar);
+        u32 check_scan = fscanf(file,"%c", &firstchar);
+        // estas al final de archivo o Error de lectura
+        if (check_scan == EOF){
+            printf("liberar memoria, retornar NULL 1\n");
+            exit(EXIT_FAILURE);
+        }
+        // la linea cumple el formato: e v w 
+        else if (firstchar == 'e' && fscanf(file,"%lu %lu", &vA, &vB) > 0){
+            // ...
+            // ... code here
+            // ...
+            printf("%lu %lu\n", vA, vB);
+            count_m++;
+            g->mlados++;
+        }
+        // la linea no lo cumple 
+        else{
+            printf("liberar memoria, retornar NULL 2\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //if (g->mlados < m)
 }
 /*
 c
